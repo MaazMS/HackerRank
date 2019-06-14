2b2cbf22ee53040850d2452b3a6155280c9e8fbd
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-06-14 03:37:42 -0700

    scanset topic

---
+++ 30Days/Day 0 Hello, world.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    
+    char input_string[105];              // Declare a variable named 'input_string' to hold our input.
+    
+    scanf("%[^\n]", input_string);      // scanset %[^\n] is allow to accecpt the input until a new line .
+    
+    printf("Hello, World.\n");
+    
+    printf("%s",input_string);
+    
+    
+    return 0;
+}
+
+/* output
+
+Hello, World.
+Welcome to 30 Days of Code!             */
+
+*/
